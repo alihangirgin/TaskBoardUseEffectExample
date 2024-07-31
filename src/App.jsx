a2f79d68e0338685978519a335828bbb4db93351
Alihan Girgin <alihangir@gmail.com>
2024-07-31 03:18:05 +0300

    implement useEffect when page refreshed

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,57 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import TaskForm from "./components/Form/TaskForm";
 import TaskList from "./components/Form/TaskList";
+import axios from "axios";
+
 
 function App() {
+
+  const getTasks = async() => {
+    const response = await axios.get("http://localhost:3000/tasks")
+    setTasks(response.data);
+  }
+
+  useEffect(()=> {
+    getTasks();
+  },[])
+
   const [tasks, setTasks] = useState([]);
 
-  const handleFormSubmit = (title, description) => {
+  const handleFormSubmit = async (title, description) => {
+
+    const response = await axios.post("http://localhost:3000/tasks", {
+      title : title,
+      description : description
+    })
+
     setTasks([
       ...tasks,
       {
-        id: Math.round(Math.random() * 99999),
+        id: response.data.id,
         title: title,
         description: description,
       },
     ]);
   };
 
-  const handleDeletedItem = (deletedItemId) => {
+  const handleDeletedItem = async(deletedItemId) => {
     const newArray = tasks.filter(x=> x.id != deletedItemId);
     setTasks(newArray);
+    await axios.delete(`http://localhost:3000/tasks/${deletedItemId}`)
   }
 
-  const handleUpdateFormParameters = (idValue, titleValue, descriptionValue) => {
+  const handleUpdateFormParameters = async(idValue, titleValue, descriptionValue) => {
     const itemToBeUpdateIndex =  tasks.findIndex(x=> x.id === idValue);
     if(itemToBeUpdateIndex != -1)
     {
       tasks[itemToBeUpdateIndex].title =  titleValue;
       tasks[itemToBeUpdateIndex].description =  descriptionValue;
       setTasks(tasks);
+      await axios.patch(`http://localhost:3000/tasks/${idValue}`, {
+        title : titleValue,
+        description : descriptionValue
+      })
     }
   }
 
